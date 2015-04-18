f2f1b11c74e59fcc8980596cfc1880bd13f1307e
ienchia <ienchia@users.noreply.github.com>
2015-04-18 20:38:02 +0700

    add hello message repetition

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+  return str_repeat('Hello', getenv('TIMES'));
 });
 
 // Register a Twig service provider

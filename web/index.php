1040d918f826d88f18081237bc525970929d073a
ienchia <ienchia@users.noreply.github.com>
2015-04-18 20:53:47 +0700

    add database demo page

--- web/index.php
+++ web/index.php
@@ -10,6 +10,17 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Add PDO connection
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"], '/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+); 
+
 // Our web handlers
 
 $app->get('/', function() use($app) {
@@ -25,6 +36,21 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/twig/{name}', function ($name) use ($app) {
   return $app['twig']->render('index.twig', array(
       'name' => $name,
+  ));
+});
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
   ));
 });
 
